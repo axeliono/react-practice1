b01d40ff4f3058b61023cdbf2d193c218f14d12c
axeliono <69375446+axeliono@users.noreply.github.com>
2022-05-18 16:31:19 -0500

    added tests

---
+++ src/components/Nav/__tests__/index.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {render, cleanup} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from '..';
+
+afterEach(cleanup);
+
+describe('Nav component', () => {
+    it('render', () => {
+        render(<Nav/>);
+    })
+
+    it('matches snapshot', () => {
+        const {asFragment} = render(<Nav />);
+
+        expect(asFragment()).toMatchSnapshot();
+    })
+})
+
+describe('emoji is visible', () => {
+     it('inserts emoji into the h2', () => {
+         const {getByLabelText}  = render(<Nav/>);
+
+         expect(getByLabelText('camera')).toHaveTextContent('📸');
+     }) 
+    })
